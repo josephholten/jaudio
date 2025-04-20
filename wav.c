665df84d0d8ae41b9d829242d48ea536ccb90790
Joseph Holten <joseph@holten.com>
2025-04-20 10:39:30 +0200

    working write riff header

--- wav.c
+++ wav.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define PACKED __attribute__((packed))
+
+struct PACKED riff_header_t {
+  char chunk_id[4];
+  int chunk_size;
+  char format[4];
+};
 
 int main() {
-  printf("hello world\n");
+  struct riff_header_t header = {
+    .chunk_id = "RIFF",
+    .chunk_size = 0, // fill in later
+    .format = "WAVE",
+  };
+
+  int fd = open("1.wav", O_RDWR | O_CREAT, 00664);
+
+  write(fd, &header, sizeof(header));
+
+  close(fd);
 }
